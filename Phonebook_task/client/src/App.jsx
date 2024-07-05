1d15ac5f0ee219af3e57514a5d6f4be3d393ae86
Ibeaka Godson <godsonibeaka@yahoo.com>
2024-07-05 03:37:37 +0100

    update

---
+++ Phonebook_task/client/src/App.jsx
@@ -0,0 +1,10 @@
+// Date: 03/18/2021
+import Sidebar from './components/sidebar';
+
+export default function App() {
+  return (
+    <div className="text-center">
+      <Sidebar />
+    </div>
+  );
+}
